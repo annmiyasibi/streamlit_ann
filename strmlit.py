0a3ca8884d925f6173dc58677266ad29fdfa562c
annmiyasibi <annmiyasibi@gmail.com>
2025-07-18 08:42:00 +0530

    Update strmlit.py

--- strmlit.py
+++ strmlit.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 st.title("upload csv")
 st.write("choose the csv file required")
-uploaded_file=st.file_uploader("browse files")
+uploaded_file=st.file_uploader("browse files",type="csv")
 if uploaded_file:
   file=pd.read_csv(uploaded_file)
   st.write(file)
