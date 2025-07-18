a646b5c5e79b358010924a1bf61d9f01ce321dfe
annmiyasibi <annmiyasibi@gmail.com>
2025-07-18 08:22:42 +0530

    Update strmlit.py

--- strmlit.py
+++ strmlit.py
@@ -1,18 +1,8 @@
 import streamlit as st
 from PIL import Image 
 
-st.title(" Simple Streamlit App")
+st.title(" welcome ")
 
 st.write("Welcome to this simple Streamlit app! ")
-
-
-image = Image.open("C:/Users/annmi/Downloads/62.jpg")
-
-
-st.image(
-    image,
-    caption="Sample Image",
-    use_column_width=True
-)
 
 st.success("This is a success message!")
